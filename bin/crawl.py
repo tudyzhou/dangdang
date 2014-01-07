bd4db2a025d0086716171ed0a8e937c904700f7f
tudy <tudyzhou@cyagen.com>
2014-01-07 12:21:09 +0800

    更新ignore

---
+++ bin/crawl.py
@@ -0,0 +1,153 @@
+#! /usr/bin/env python
+
+USAGE = '''
+    USAGE:
+        %s <id_lis.txt> <output.txt>
+''' % __file__
+
+import os
+import sys
+import re
+import time
+import urllib2
+
+from lib_detail import ParserDetail
+
+
+P_img = re.compile(r'img src=\'(.+\d+-1_l\.jpg)')
+P_item = re.compile(r'/(\d{3,})-1_l\.jpg$')
+
+def Debug(msg, out=sys.stderr):
+    if out != sys.stderr:
+        print >> out, msg
+    print >> sys.stderr, msg
+
+
+def parserDB(_f, debug):
+    for line in open(_f):
+        line = line.rstrip()
+        if not line:
+            continue
+        if line[0] == "#":
+            continue
+        yield line
+
+
+def fetchPIC(url, out_p, debug):
+    img_url = re.sub(r'1_l.jpg$', '1_e.jpg', url)
+    img = ''
+
+    try:
+        img_r = urllib2.urlopen(img_url)
+        img = img_r.read()
+        open(out_p, "wb").write(img)
+    except Exception as e:
+        Debug("[ERR] Fetch %s: %s" % (img_url, e))
+
+    return len(img)
+
+
+def fetchRecord(_id, img_out_p, debug):
+    url = "http://search.dangdang.com/?key=%s" % _id
+
+    try:
+        response = urllib2.urlopen(url)
+        html = response.read()
+    except Exception as e:
+        Debug("[ERR] Fetch %s: %s" % (url, e))
+
+    ret = ''
+    p = P_img.search(html)
+    if p:
+        ret = p.group(1)
+        fetchPIC(ret, img_out_p, debug)
+    
+    return ret, url
+
+
+def assertDir(_dir):
+    if not os.path.isdir(_dir):
+        os.mkdir(_dir)
+
+
+def fetchDetail(img_url, cache_dir, debug):
+    item_id = P_item.search(img_url).group(1)
+    (ret, err) = ParserDetail(item_id, cache_dir)
+    if err:
+        Debug("[ERR] Fetch dangdang item %s FAIL. %s" % (item_id, img_url), debug)
+
+    return ret
+
+
+def main(db_p, output_p):
+    base_p = re.sub(r'\.txt', '', output_p)
+
+    cache_p = "%s_cache" % (base_p)
+    index_p = "%s_index" % (base_p)
+    recom_p = "%s_recom" % (base_p)
+
+    assertDir(base_p)
+    assertDir(cache_p)
+    assertDir(index_p)
+    assertDir(recom_p)
+
+    output = open(output_p, "w")
+    debug_p = base_p + ".debug.txt"
+    debug = open(debug_p, "w")
+
+    r_fmt = lambda x: x if x else "-"
+
+    for _id in parserDB(db_p, debug):
+
+        img_out = os.path.join(base_p, "%s.jpg" % _id)
+        index_out = os.path.join(index_p, "%s.txt" % _id)
+        recom_out = os.path.join(recom_p, "%s.txt" % _id)
+        if os.path.isfile(img_out) \
+                and os.path.isfile(index_out) \
+                and os.path.isfile(recom_out):
+            Debug("[DETAIL] %s record exist in '%s', conitnue next" % (_id, img_out))
+            continue
+
+        details = {}
+        matchs = [0,0,0]
+        status = "FAIL"
+        prefix = "ERR"
+        start_t = time.time()
+        (img_url, search_url) = fetchRecord(_id, img_out, debug)
+
+
+        if img_url:
+            status = "WARM"
+            matchs[0] = 1
+
+            details = fetchDetail(img_url, cache_p, debug)
+            index_con = details.get("index", None)
+            recom_con = details.get("recom", None)
+
+            if index_con:
+                matchs[1] = 1
+                open(index_out, 'w').write(index_con)
+            if recom_con:
+                matchs[2] = 1
+                open(recom_out, 'w').write(recom_con)
+
+            if sum(matchs) == 3:
+                status = "OK"
+                prefix = "DETAIL"
+            matchs = map(str, matchs)
+
+        Debug("[%s] Fetch %s, %s %.2f second. %s " % (
+            prefix, _id, '-'.join(matchs), time.time()-start_t, status), debug)
+
+        output.write("%s\t%s\t%s\n" % (_id, '\t'.join(matchs), r_fmt(img_url)))
+
+    
+    debug.close()
+    output.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        main(*sys.argv[1:])
+    else:
+        print USAGE
---
+++ bin/lib_detail.py
@@ -0,0 +1,84 @@
+#-*- coding: utf-8 -*
+
+USAGE = '''
+    ParserDetail lib
+'''
+
+INPUT_CODE = 'gbk'                  # html encode
+OUTPUT_CODE = 'utf-8'               # output encode for databases
+
+
+import os
+import re
+import urllib2
+
+
+CATEGORIES = {
+    "recom": '内容推荐',
+    "index": '目录',
+    }
+
+P_CONTENT = re.compile(r'<textarea style="height:0px;border-width:0px;">\s*(?P<content>.*)\s*', re.DOTALL)
+
+
+# Return utf-8 encode content
+def fetchDangDang(dangdang_id, cache_dir, input_code):
+    url = "http://product.dangdang.com/%s.html" % dangdang_id
+    con = ''
+    err = ''
+
+    cache_path = ''
+    if cache_dir:
+        cache_path = os.path.join(cache_dir, "%s.html" % dangdang_id)
+    if cache_path:
+        if os.path.isfile(cache_path):
+            con = open(cache_path).read()
+            con = con.decode(input_code)
+            con = con.encode('utf-8')
+
+    if not con:
+        try:
+            con_r = urllib2.urlopen(url)
+            con = con_r.read()
+            if cache_path:
+                open(cache_path, "w").write(con)
+            con = con.decode(input_code)
+            con = con.encode('utf-8')
+        except Exception as e:
+            err = e
+
+    return (con, err)
+
+
+# convert a dangdang item id to details
+def ParserDetail(dangdang_id, cache_dir, out_code=OUTPUT_CODE):
+
+    ret = {}
+    (content, err) = fetchDangDang(dangdang_id, cache_dir, INPUT_CODE)
+
+    for key in CATEGORIES:
+        con = ''
+        word = CATEGORIES[key]
+        s = content.find(word)
+        if s != -1:
+            tmp = content[s:]
+            e = tmp.find("</textarea>")
+            if e != -1:
+                tmp = tmp[0:e].rstrip()
+                p = P_CONTENT.search(tmp)
+                if p:
+                    con = p.group("content")
+        con = con.decode('utf-8')
+        con = con.encode(OUTPUT_CODE)
+        ret[key] = con
+
+    return (ret, err)
+
+
+# test
+if __name__ == "__main__":
+    (res, err) = ParserDetail("22640661", "cache")
+    if err:
+        print err
+    else:
+        open("test.txt", "w").write("\n".join(["[%s]:\n%s" % (x, res[x]) for x in res]))
